b4021e520757b63a6cb435dc9619188863fddf82
vopenko-dev <vopenko84@yandex.com>
2023-08-02 13:39:06 +0000

    Add finalized taski before review.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,30 @@
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
+sentry_sdk.init(
+    dsn="https://527136c928e1b166543b1958ec1b76b8@o4505635169959936.ingest.sentry.io/4505635176448000",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '51.250.105.25',
+    '127.0.0.1',
+    'localhost',
+    'violetcow.ddns.net'
+   ]
 
 
 # Application definition
@@ -103,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static_backend'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,10 +4,14 @@ from rest_framework import routers
 
 from api import views
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
